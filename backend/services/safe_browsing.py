dd91a32ed2c084aa3d49a5d7ceda0f553fa94e92
oudijk <justin.oudijk@hotmail.com>
2026-03-28 13:04:09 -0400

    Updated safe_browsing.py test

--- backend/services/safe_browsing.py
+++ backend/services/safe_browsing.py
@@ -1,1 +1,9 @@
 # Google safe browsing API
+# backend/services/safe_browsing.py
+
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_SAFE_BROWSING_API = os.getenv("GOOGLE_SAFE ")
